b3bb41213f567a8e8172d3f665f81252643ab6ff
ammarm08 <ammar.mian08@gmail.com>
2015-12-12 03:16:27 -0500

    <devops> minifying css to include margin adjustments

--- dist/app.js
+++ dist/app.js
@@ -108,6 +108,7 @@ angular.module('graffiti.songs', [])
 
     $scope.data = {note: "Whoops, something went wrong. Go home and search again!"};
     $scope.image = "";
+    $scope.current = { artist: "", title: "" }
 
     if (API.get()) {
       $scope.data = API.get();
@@ -121,6 +122,9 @@ angular.module('graffiti.songs', [])
     }
 
     $scope.getAnnotations = function(id, title, img) {
+
+      $scope.current.title = title;
+      $scope.current.artist = $scope.data.artist + ": ";
 
       $scope.data.song = title;
       $scope.data.video = "";
